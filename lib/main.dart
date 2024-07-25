991d068099c07e8cf8c746da8901b619ea7394f5
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-07-25 12:46:25 +0300

    app main

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:azkar/features/home/home_view.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      builder: (context) => const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(630, 1280),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          useMaterial3: false,
+        ),
+        home: const HomeView(),
+      ),
+    );
+  }
+}
