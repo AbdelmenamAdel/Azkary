26e7ad5d80f6db590ba3eb98b58fb1625bcd3f03
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-07-28 22:30:52 +0300

    update zekr model

---
+++ lib/features/Azkar/manager/model/zekr_model.dart
@@ -0,0 +1,13 @@
+class ZekrModel {
+  String? title;
+  String zekr;
+  String? leading;
+  int loop;
+
+  ZekrModel({
+    this.title,
+    required this.zekr,
+    this.leading,
+    required this.loop,
+  });
+}
