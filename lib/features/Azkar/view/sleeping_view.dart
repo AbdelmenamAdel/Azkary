96ea32fce15286ba1da48763b69bda5912f2edff
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-07-31 15:12:15 +0300

    sleeping view done

---
+++ lib/features/Azkar/view/sleeping_view.dart
@@ -0,0 +1,41 @@
+import 'package:azkar/core/utils/app_colors.dart';
+import 'package:azkar/core/utils/azkar.dart';
+import 'package:azkar/features/Azkar/manager/Counter/zekr_counter_cubit.dart';
+import 'package:azkar/features/Azkar/manager/Counter/zekr_counter_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'widgets/zekr_card.dart';
+
+class SleepingAzkarView extends StatelessWidget {
+  const SleepingAzkarView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.primary,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(),
+          backgroundColor: AppColors.primary,
+          body: BlocBuilder<ZekrCounterCubit, ZekrCounterState>(
+            builder: (context, state) {
+              var cubit = ZekrCounterCubit.get(context);
+              return ListView.builder(
+                padding: const EdgeInsets.only(top: 5, left: 5, right: 5),
+                itemBuilder: (context, index) {
+                  return ZekrCardWidget(
+                    model: cubit.sleeping[index],
+                    zekrName: 'sleeping',
+                    sort: index + 1,
+                  );
+                },
+                itemCount: Azkar().sleeping.length,
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
