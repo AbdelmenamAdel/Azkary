825310381dd62f79f0537e85f9c1abb4e4abd2f6
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-01 12:12:42 +0300

    goame3 eldoaa done

---
+++ lib/features/Azkar/view/goame3_eldo3a_view.dart
@@ -0,0 +1,39 @@
+import 'package:azkar/core/utils/app_colors.dart';
+import 'package:azkar/core/utils/azkar.dart';
+import 'package:azkar/features/Azkar/manager/Counter/zekr_counter_cubit.dart';
+import 'package:azkar/features/Azkar/manager/Counter/zekr_counter_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'widgets/zekr_card.dart';
+
+class Goame3Eldo3aView extends StatelessWidget {
+  const Goame3Eldo3aView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.primary,
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: AppColors.primary,
+          body: BlocBuilder<ZekrCounterCubit, ZekrCounterState>(
+            builder: (context, state) {
+              var cubit = ZekrCounterCubit.get(context);
+              return ListView.builder(
+                padding: const EdgeInsets.only(top: 5, left: 5, right: 5),
+                itemBuilder: (context, index) {
+                  return ZekrCardWidget(
+                    model: cubit.goame3Eldo3a[index],
+                    zekrName: 'goame3Eldo3a',
+                    sort: index + 1,
+                  );
+                },
+                itemCount: Azkar().goame3Eldo3a.length,
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
