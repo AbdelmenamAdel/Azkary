43b42c1c924fbad4280bc549497a5ffdf7e90085
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-01 13:53:23 +0300

    customize these images

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -3,10 +3,15 @@ class AppImages {
   static const String hanpDoaa = "assets/images/hand_doaa.png";
   static const String openHand = "assets/images/open-hands.png";
   static const String rosary = "assets/images/rosary.png";
+  static const String rosary1 = "assets/images/rosary1.png";
   static const String nightMode = "assets/images/night-mode.png";
   static const String sunny = "assets/images/love_1.png";
   static const String heart = "assets/images/heart.png";
   static const String sleepMoon = "assets/images/sleep.png";
   static const String love = "assets/images/love.png";
   static const String eRosary = "assets/images/e_rosary.png";
+  static const String pray = "assets/images/pray.png";
+  static const String allah = "assets/images/allah.png";
+  static const String allah1 = "assets/images/allah1.png";
+  static const String prayer = "assets/images/prayer.png";
 }
