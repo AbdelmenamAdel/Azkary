3563392c2fd4db4fd52027f7175d2b6dfd54f4d0
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-01 12:14:14 +0300

    add inkwell for image doaa

--- lib/features/home/widgets/doaa_image_section.dart
+++ lib/features/home/widgets/doaa_image_section.dart
@@ -1,4 +1,5 @@
 import 'package:azkar/core/utils/app_images.dart';
+import 'package:azkar/features/Azkar/view/widgets_without_counter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -12,11 +13,38 @@ class DoaaImageSection extends StatelessWidget {
       child: ClipRRect(
         clipBehavior: Clip.antiAliasWithSaveLayer,
         borderRadius: BorderRadius.circular(10),
-        child: Image.asset(
-          AppImages.doaa,
-          height: 380.h,
-          width: double.infinity,
-          fit: BoxFit.fill,
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return const WidgetsWithoutCounter(
+                    zekrName: "fadlEldo3a",
+                  );
+                },
+              ),
+            );
+          },
+          child: Stack(
+            children: [
+              Image.asset(
+                AppImages.doaa,
+                height: 380.h,
+                width: double.infinity,
+                fit: BoxFit.fill,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "فضل الدعاء",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
