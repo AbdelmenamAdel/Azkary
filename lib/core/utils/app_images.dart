574c4385d62615ba502c27ad19aa993d35e9210f
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-02 17:31:55 +0300

    add new images

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -2,7 +2,6 @@ class AppImages {
   static const String doaa = "assets/images/doaa.png";
   static const String hanpDoaa = "assets/images/hand_doaa.png";
   static const String openHand = "assets/images/open-hands.png";
-  static const String rosary = "assets/images/rosary.png";
   static const String rosary1 = "assets/images/rosary1.png";
   static const String nightMode = "assets/images/night-mode.png";
   static const String sunny = "assets/images/love_1.png";
@@ -16,4 +15,13 @@ class AppImages {
   static const String allahnames = "assets/images/allahnames.webp";
   static const String prayer = "assets/images/prayer.png";
   static const String quran = "assets/images/quran.png";
+  static const String image1 = "assets/images/a1.jpg";
+  static const String image2 = "assets/images/a2.jpg";
+  static const String image3 = "assets/images/a3.jpg";
+  static const String image4 = "assets/images/a4.jpg";
+  static const String image5 = "assets/images/a5.jpg";
+  static const String image7 = "assets/images/image7.png";
+  static const String appLogo = "assets/images/a6.jpg";
+  static const String image8 = "assets/images/a8.jpg";
+  static const String image9 = "assets/images/a9.jpg";
 }
---
+++ lib/features/Azkar/view/algmo3ah_view.dart
@@ -0,0 +1,59 @@
+import 'package:azkar/core/common/custom_appbar.dart';
+import 'package:azkar/core/common/custom_zoom_Image.dart';
+import 'package:azkar/core/utils/app_colors.dart';
+import 'package:azkar/core/utils/app_images.dart';
+import 'package:flutter/material.dart';
+
+class Algmo3ahView extends StatelessWidget {
+  const Algmo3ahView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.primary,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: customAppbar(context),
+          backgroundColor: AppColors.primary,
+          body: const Padding(
+            padding: EdgeInsets.all(24.0),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  CustomZoomImage(
+                    tag: 'image1',
+                    image: AppImages.image1,
+                  ),
+                  CustomZoomImage(
+                    tag: 'image4',
+                    image: AppImages.image4,
+                  ),
+                  CustomZoomImage(
+                    tag: 'image5',
+                    image: AppImages.image5,
+                  ),
+                  CustomZoomImage(
+                    tag: 'image7',
+                    image: AppImages.image7,
+                  ),
+                  CustomZoomImage(
+                    tag: 'image2',
+                    image: AppImages.image2,
+                  ),
+                  CustomZoomImage(
+                    tag: 'image3',
+                    image: AppImages.image3,
+                  ),
+                  CustomZoomImage(
+                    tag: 'image9',
+                    image: AppImages.image9,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
