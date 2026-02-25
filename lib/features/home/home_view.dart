8c54eeda0e131dd7f9c08591a386932a7dcc2b0b
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2026-02-25 08:50:30 +0200

    remove test

--- lib/features/home/home_view.dart
+++ lib/features/home/home_view.dart
@@ -404,30 +404,30 @@ class _HomeViewState extends State<HomeView> {
                           _showThemeDialog(context);
                         },
                       ),
-                      // if (false)
-                      ListTile(
-                        leading: Icon(
-                          Icons.notifications_active,
-                          color: colors.secondary,
-                        ),
-                        title: Text(
-                          "تجربة التنبيهات",
-                          style: TextStyle(
-                            fontSize: 12,
-                            fontFamily: 'Cairo',
-                            color: context.colors.text,
+                      if (false)
+                        ListTile(
+                          leading: Icon(
+                            Icons.notifications_active,
+                            color: colors.secondary,
                           ),
+                          title: Text(
+                            "تجربة التنبيهات",
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontFamily: 'Cairo',
+                              color: context.colors.text,
+                            ),
+                          ),
+                          trailing: Icon(
+                            Icons.play_arrow,
+                            color: colors.text?.withValues(alpha: 0.5),
+                            size: 16,
+                          ),
+                          onTap: () {
+                            Navigator.pop(context);
+                            sl<NotificationService>().testNotification();
+                          },
                         ),
-                        trailing: Icon(
-                          Icons.play_arrow,
-                          color: colors.text?.withValues(alpha: 0.5),
-                          size: 16,
-                        ),
-                        onTap: () {
-                          Navigator.pop(context);
-                          sl<NotificationService>().testNotification();
-                        },
-                      ),
                       Divider(color: colors.text?.withValues(alpha: 0.1)),
                       ListTile(
                         leading: Icon(
