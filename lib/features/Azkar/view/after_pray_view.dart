6995ad0761961af09b63ab07f4353c6a83d2f434
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2026-03-10 17:50:29 +0200

    feat: Implement app name localization for both Android and iOS, and reorder imports in `after_pray_view.dart`.

--- lib/features/Azkar/view/after_pray_view.dart
+++ lib/features/Azkar/view/after_pray_view.dart
@@ -2,10 +2,10 @@ import 'package:azkar/core/common/custom_appbar.dart';
 import 'package:azkar/core/theme/app_colors_extension.dart';
 import 'package:azkar/core/utils/azkar.dart';
 import 'package:azkar/features/Azkar/manager/Counter/zekr_counter_cubit.dart';
+import 'package:azkar/features/Azkar/manager/Counter/zekr_counter_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../manager/Counter/zekr_counter_state.dart';
 import 'widgets/zekr_card.dart';
 
 class AfterPrayAzkarView extends StatelessWidget {
